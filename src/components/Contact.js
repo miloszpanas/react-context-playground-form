be27c11400eecc459fbfc699324361e50f1d3c65
Miłosz <milosz@localhost.localdomain>
2019-02-18 18:53:36 +0100

    Initialize project, first component

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h3>Some data to display</h3>
+      <ul>
+        <li>Name:</li>
+        <li>Surnameame:</li>
+        <li>Email:</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
